82e61fd6f8e6e1856851278a686824132b28244a
Zhao Lei <aicoylei@gmail.com>
2010-11-04 22:12:38 +0800

    make tmp dir, chmod 777

---
+++ init_env.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+mkdir public/protected/runtime
+mkdir public/protected/data
+mkdir public/assets
+chmod -R 777 public/protected/runtime
+chmod -R 777 public/protected/data
+chmod -R 777 public/assets
